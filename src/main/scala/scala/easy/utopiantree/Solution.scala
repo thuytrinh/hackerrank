73a1cef7fce1621523a679f968a208490d23810e
Thuy Trinh <thuy@skedgo.com>
2015-04-20 22:52:26 +0700

    Submit 'Utopian Tree'

---
+++ src/main/scala/scala/easy/utopiantree/Solution.scala
@@ -0,0 +1,18 @@
+package scala.easy.utopiantree
+
+/**
+ * https://www.hackerrank.com/challenges/utopian-tree
+ */
+object Solution {
+  def main(args: Array[String]): Unit = {
+    val testCount = readInt()
+    (1 to testCount).map(_ => {
+      val n = readInt()
+      var h = 1
+      (0 until n).foreach(s => {
+        h += (if (s % 2 == 0) h else 1)
+      })
+      h
+    }).foreach(println)
+  }
+}

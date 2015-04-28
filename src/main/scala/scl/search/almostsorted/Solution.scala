b4b960b4c164e85adb2dec9536407f4f0ef71467
Thuy Trinh <thuy@skedgo.com>
2015-04-28 17:59:56 +0700

    Phuc yeah! Accepted! Such epic feeling today!

--- src/main/scala/scl/search/almostsorted/Solution.scala
+++ src/main/scala/scl/search/almostsorted/Solution.scala
@@ -6,33 +6,68 @@ package scl.search.almostsorted
 object Solution {
   def main(args: Array[String]) {
     readInt()
-    val list = readLine().split(" ").map(_.toInt).toList
-    println(canSort(list))
+    val numbers = readLine().split(" ").map(_.toInt)
+    println(canSort(numbers))
   }
 
-  def canSort(list: List[Int]): String = {
-    if (list.length == 2 && list.head > list.last) {
+  def canSort(numbers: Array[Int]): String = {
+    if (numbers.length == 2 && numbers.head > numbers.last) {
       "yes\nswap 1 2\n"
     } else {
-      val unsorted = (0 until list.length - 1).map(i => {
-        if (list(i) > list(i + 1)) (i, 1) else (i, 0)
-      }).filter(p => p._2 == 1).toList
-      unsorted.length match {
-        case l if l < 2 => "no\n"
-        case l if l == 2 =>
-          "yes\nswap %d %d\n".format(
-            unsorted.head._1 + 1,
-            unsorted.last._1 + 2
-          )
-        case _ =>
-          if ((unsorted.last._1 - (unsorted.head._1 - 1)) == unsorted.length) {
-            "yes\nreverse %d %d\n".format(
-              unsorted.head._1 + 1,
-              unsorted.last._1 + 2
-            )
+      var unsortedPairCount = 0
+      var firstUnsortedPairIndex = -1
+      var finalUnsortedPairIndex = -1
+
+      // -1: We know nothing yet.
+      // 0: Likely to be swappable.
+      // 1: Likely to be reversible.
+      // 2: Nope! Need multiple operations to sort it.
+      var prediction = -1
+      for (i <- 0 until numbers.length - 1 if prediction != 2) {
+        if (numbers(i) > numbers(i + 1)) {
+          // We've seen an unsorted pair.
+          // What's we gonna do now?
+          unsortedPairCount += 1
+          if (finalUnsortedPairIndex == -1) {
+            // Is that first time ever?
+            finalUnsortedPairIndex = i
+            firstUnsortedPairIndex = i
+
+            // Narrow the prediction.
+            // If it's the only unsorted pair in the array,
+            // just swap the pair, then the array is sorted.
+            // But there's an exception. For instance, [3,1,2,4].
+            // (3,1) is that pair but swapping it helps nothing.
+            prediction = 0
+          } else if (finalUnsortedPairIndex == i - 1) {
+            // We've found yet another unsorted pair
+            // standing next to the previous unsorted pair.
+            finalUnsortedPairIndex = i
+            if (unsortedPairCount == 2) {
+              // We've seen only 2 unsorted pairs
+              // standing next to each other.
+              // Due to problem statement, it's more likely
+              // to be swappable than reversible.
+              prediction = 0
+            } else {
+              // Wow! This is a form of desc-sorted sub-array.
+              prediction = 1
+            }
+          } else if (unsortedPairCount == 2) {
+            finalUnsortedPairIndex = i
+            prediction = 0
           } else {
-            "no\n"
+            prediction = 2
           }
+        }
+      }
+
+      if (prediction == 0 && firstUnsortedPairIndex > 0) {
+        "yes\nswap %d %d\n".format(firstUnsortedPairIndex + 1, finalUnsortedPairIndex + 2)
+      } else if (prediction == 1) {
+        "yes\nreverse %d %d\n".format(firstUnsortedPairIndex + 1, finalUnsortedPairIndex + 2)
+      } else {
+        "no\n"
       }
     }
   }
--- src/test/scala/scl/search/almostsorted/Solution$Test.scala
+++ src/test/scala/scl/search/almostsorted/Solution$Test.scala
@@ -4,18 +4,26 @@ import org.scalatest.FunSuite
 
 class Solution$Test extends FunSuite {
   test("testCanSort1") {
-    assert(Solution.canSort(List(4, 2)) == "yes\nswap 1 2\n")
+    assert(Solution.canSort(Array(4, 2)) == "yes\nswap 1 2\n")
   }
 
   test("testCanSort2") {
-    assert(Solution.canSort(List(3, 1, 2)) == "no\n")
+    assert(Solution.canSort(Array(3, 1, 2)) == "no\n")
   }
 
   test("testCanSort3") {
-    assert(Solution.canSort(List(1, 5, 4, 3, 2, 6)) == "yes\nreverse 2 5\n")
+    assert(Solution.canSort(Array(1, 5, 4, 3, 2, 6)) == "yes\nreverse 2 5\n")
   }
 
   test("testCanSort4") {
-    assert(Solution.canSort(List(1, 2, 10, 4, 7, 8, 3, 12, 14)) == "yes\nswap 3 7\n")
+    assert(Solution.canSort(Array(1, 2, 10, 4, 7, 8, 3, 12, 14)) == "yes\nswap 3 7\n")
+  }
+
+  test("testCanSort5") {
+    assert(Solution.canSort(Array(1, 10, 4, 14)) == "yes\nswap 2 3\n")
+  }
+
+  test("testCanSort6") {
+    assert(Solution.canSort(Array(3, 1, 2, 4)) == "no\n")
   }
 }

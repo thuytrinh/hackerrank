730f95aecddd199a54e58ad614a1c5ed202ff8cc
Thuy Trinh <thuy@skedgo.com>
2015-05-14 00:29:15 +0700

    Submit 'The Coin Change'

--- src/main/scala/scl/dp/coinchange/Solution.scala
+++ src/main/scala/scl/dp/coinchange/Solution.scala
@@ -10,12 +10,25 @@ object Solution {
     println(countWays(n, coins))
   }
 
-  def countWays(n: Int, coins: List[Int]): Int = {
-    if (n == 0) 1
-    else if (n < 0) 0
-    else coins match {
-      case Nil => 0
-      case h :: t => countWays(n - h, coins) + countWays(n, t)
+  def countWays(n: Int, coins: List[Int]): Long = {
+    val d = Array.ofDim[Long](coins.length + 1, n + 1)
+    (0 to coins.length).foreach(d(_)(0) = 1)
+
+    for (c <- 1 to coins.length; i <- 0 to n) {
+      val sub = d(c)(i)
+      val coin = coins(c - 1)
+      if (i + coin <= n) {
+        d(c)(i + coin) = d(c)(i + coin) + sub
+      }
+
+      if (i > 0 && c < coins.length) {
+        d(c + 1)(i) = d(c + 1)(i) + sub
+      }
     }
+
+    // For debugging purpose only.
+    // d.foreach(x => println(x.mkString(" ")))
+
+    d(coins.length)(n)
   }
 }
--- src/test/scala/scl/dp/coinchange/Solution$Test.scala
+++ src/test/scala/scl/dp/coinchange/Solution$Test.scala
@@ -3,12 +3,22 @@ package scl.dp.coinchange
 import org.scalatest.FunSuite
 
 class Solution$Test extends FunSuite {
-  test("testCountWays") {
+  test("(5, List(5)) == 1") {
     assert(Solution.countWays(5, List(5)) == 1)
+  }
+  test("(4, List(1, 2, 3)) == 4") {
     assert(Solution.countWays(4, List(1, 2, 3)) == 4)
+  }
+  test("(5, List(2, 3)) == 1") {
     assert(Solution.countWays(5, List(2, 3)) == 1)
+  }
+  test("(5, List(1, 3, 4)) == 3") {
     assert(Solution.countWays(5, List(1, 3, 4)) == 3)
+  }
+  test("(5, List(1, 3)) == 2") {
     assert(Solution.countWays(5, List(1, 3)) == 2)
+  }
+  test("(10, List(2, 5, 3, 6)) == 5") {
     assert(Solution.countWays(10, List(2, 5, 3, 6)) == 5)
   }
 }

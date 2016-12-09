2d0c899387438eb6b8a7c440421998af354c4031
Thuy Trinh <thuy@skedgo.com>
2016-12-10 02:52:04 +0700

    Solve the non-adjacent minimal pair

--- src/main/java/codility/inStaffJobsGmbH/test3/Solution.java
+++ src/main/java/codility/inStaffJobsGmbH/test3/Solution.java
@@ -1,54 +1,51 @@
 package codility.inStaffJobsGmbH.test3;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import static java.lang.Math.abs;
-import static java.lang.Math.min;
 
 class Solution {
   public int solution(int[] A) {
-    // return bruteForce(A);
-    // return dealWithDuplicates(A);
-    // If we find a solution for a sub-problem,
-    // how do we find solution for its super problem?
-    // Given, A and we got P and Q.
-    // Of course, abs(P-Q) is always > 1.
-    // How do we find solution for the array A::e?
-    // Given eI = indexOf(e).
-    // Given a = A[P] + e if abs(eI - P) > 1.
-    // Given b = A[Q] + e if abs(eI - Q) > 1.
-    // => newSolution = max(A[P]+A[Q], a, b) right?
-    int p = 0;
-    int q = 2;
-    int minI = p;
-    int m = A[p] + A[q];
-    for (int i = 0; i < 3; i++) {
-      if (A[minI] > A[i]) {
-        minI = i;
-      }
-    }
-    final int[] ints = {p, q};
-    for (int i = q + 1; i < A.length; i++) {
-      int a = abs(i - p) > 1 ? (A[p] + A[i]) : Integer.MAX_VALUE;
-      int b = abs(i - q) > 1 ? (A[q] + A[i]) : Integer.MAX_VALUE;
-      int c = abs(i - minI) > 1 ? (A[minI] + A[i]) : Integer.MAX_VALUE;
-      int oldM = m;
-      m = min(min(a, c), min(b, m));
-      if (m == a && a != oldM) q = i;
-      else if (m == b && b != oldM) {
-        p = q;
-        q = i;
-      }
-      ints[0] = p;
-      ints[1] = q;
-      for (int e : ints) {
-        if (A[minI] > A[e]) minI = e;
-      }
-    }
-    return m;
+    // Takes O(1) for both time and space complexities.
+    return faster(A);
   }
 
-  private int bruteForce(int[] A) {
+  private int faster(final int[] A) {
+    // Find indices of 4 smallest elements.
+    final Set<Integer> visits = new HashSet<>();
+    while (visits.size() < 4) {
+      int e = -1;
+      for (int i = 0; i < A.length; i++) {
+        if (!visits.contains(i)) {
+          if (e == -1 || A[e] > A[i]) {
+            e = i;
+          }
+        }
+      }
+      visits.add(e);
+    }
+    final List<Integer> indices = new ArrayList<>(visits);
+
+    // Pick non-adjacent pair which forms minimal sum.
+    int minSum = Integer.MAX_VALUE;
+    for (int i = 0; i < indices.size(); i++) {
+      for (int j = i + 1; j < indices.size(); j++) {
+        if (abs(indices.get(i) - indices.get(j)) > 1) {
+          int s = A[indices.get(i)] + A[indices.get(j)];
+          if (minSum > s) minSum = s;
+        }
+      }
+    }
+    return minSum;
+  }
+
+  /**
+   * To perform stress-tests only.
+   */
+  int bruteForce(int[] A) {
     int m = Integer.MAX_VALUE;
     for (int i = 0; i < A.length; i++) {
       for (int j = i + 2; j < A.length; j++) {
@@ -58,35 +55,5 @@ class Solution {
       }
     }
     return m;
-  }
-
-  private int dealWithDuplicates(int[] A) {
-    int min0 = Integer.MAX_VALUE;
-    for (int aA : A) {
-      if (min0 > aA) {
-        min0 = aA;
-      }
-    }
-    final LinkedList<Integer> is = new LinkedList<>();
-    for (int i = 0; i < A.length; i++) {
-      if (min0 == A[i]) {
-        is.add(i);
-      }
-    }
-    int s = Integer.MAX_VALUE;
-    for (int mini0 : is) {
-      int min1 = Integer.MAX_VALUE;
-      for (int i = 0; i < A.length; i++) {
-        if (abs(mini0 - i) > 1) {
-          if (min1 > A[i]) {
-            min1 = A[i];
-          }
-        }
-      }
-      if (s > min0 + min1) {
-        s = min0 + min1;
-      }
-    }
-    return s;
   }
 }
--- src/test/java/codility/inStaffJobsGmbH/test3/SolutionTest.java
+++ src/test/java/codility/inStaffJobsGmbH/test3/SolutionTest.java
@@ -2,6 +2,9 @@ package codility.inStaffJobsGmbH.test3;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SolutionTest {
@@ -11,16 +14,24 @@ public class SolutionTest {
   }
 
   @Test
-  public void test() {
+  public void stressTest() {
     final Solution s = new Solution();
-    assertThat(s.solution(new int[]{5, 2, 4, 6, 3, 7})).isEqualTo(5);
-    assertThat(s.solution(new int[]{2, 2, 1, 6, 3, 7})).isEqualTo(3);
-    assertThat(s.solution(new int[]{2, 2, 1, 0, 3, 7})).isEqualTo(2);
-    assertThat(s.solution(new int[]{2, 2, 0, 1, 1, 7})).isEqualTo(1);
-    assertThat(s.solution(new int[]{1, 0, 2, 1, 5, 7})).isEqualTo(1);
-    assertThat(s.solution(new int[]{2, 0, 0, 3, 4})).isEqualTo(2);
-    assertThat(s.solution(new int[]{3, 0, 0, 2, 4})).isEqualTo(2);
-    assertThat(s.solution(new int[]{0, 0, 2, 4, 2})).isEqualTo(2);
-    assertThat(s.solution(new int[]{0, 0, 0, 0, 2})).isEqualTo(0);
+    for (int j = 0; j < 1000; j++) {
+      int n = 1000;
+      final int[] ints = new int[n];
+      for (int i = 0; i < ints.length; i++) {
+        ints[i] = ThreadLocalRandom.current().nextInt(0, 101);
+      }
+      assertThat(s.solution(ints))
+          .describedAs(Arrays.toString(ints))
+          .isEqualTo(s.bruteForce(ints));
+    }
+  }
+
+  @Test
+  public void singleTest() {
+    final Solution s = new Solution();
+    assertThat(s.solution(new int[]{44, 35, 80, 69, 90, 39, 83, 26, 17, 18}))
+        .isEqualTo(44);
   }
 }
